5500dd29ad6cc2b94fa25006e9bb4f44d3470c69
Leslie747401 <leslie747401@gmail.com>
2024-02-14 05:57:09 +0530

    resolved error submit btn

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 
   return (
    <form className="w-screen h-screen flex flex-col gap-10 justify-center items-center">
-      <input type="text" className="px-5 py-3 w-[400px] bg-white rounded-xl border border-[#b4b4b4] outline-none"/>
-      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+      <input type="text" className="px-5 py-3 w-[400px] bg-white rounded-xl border border-[#b4b4b4] outline-none" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer"/>
    </form>
   );
 }
