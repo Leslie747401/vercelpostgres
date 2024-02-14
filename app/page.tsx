d019b7e92560efc165aa49c11c26c2f36fc80edc
Leslie747401 <leslie747401@gmail.com>
2024-02-14 05:46:46 +0530

    Added useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
+import { useState } from "react";
+
 export default function Home() {
+
+  const [userInput,setUserInput] = useState('');
+
   return (
    <form className="w-screen h-screen flex flex-col gap-10 justify-center items-center">
       <input type="text" className="px-5 py-3 w-[400px] bg-white rounded-xl border border-[#b4b4b4] outline-none"/>
-      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer"/>
+      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
    </form>
   );
 }
