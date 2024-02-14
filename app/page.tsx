510dea6a4c19e9d5f7d7d35a43c4c22966dea31a
Leslie747401 <leslie747401@gmail.com>
2024-02-14 05:54:12 +0530

    edited submit btn

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
    <form className="w-screen h-screen flex flex-col gap-10 justify-center items-center">
       <input type="text" className="px-5 py-3 w-[400px] bg-white rounded-xl border border-[#b4b4b4] outline-none"/>
-      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer" value={userInput} onChange={(e) => setUserInput(e.target.value)}>Submit</input>
+      <input type="submit" className="bg-black px-4 py-2 text-white rounded-lg cursor-pointer" value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
    </form>
   );
 }
